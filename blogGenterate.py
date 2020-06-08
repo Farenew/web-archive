499f05aaea8721e1bd0039e4c09f3d7dc0ad002f
farenew <jiang-feng@outlook.com>
2020-06-08 10:35:52 +0800

    重构了布局和生成代码

--- blogGenterate.py
+++ blogGenterate.py
@@ -21,80 +21,76 @@ iniDir = os.getcwd()
 # current date and time
 date = datetime.now().strftime('%Y-%m-%dT%H:%M:%S') + "+08:00"
 
-# content dirs
-scienceDir = "./content/science/"
-socialDir = "./content/social/"
-newsDir = "./content/news/"
 
-scienceList = list()
-socialList = list()
-newsList = list()
-
-# 建立content目录和内容的联系
+# 构建关于content文件夹的内容列表，按照字典形式存储，字典key是文件夹名称，value是文件夹内容（包括子文件夹内容）
+contentDir = "./content"
 contentDict = dict()
-contentDict[scienceDir] = scienceList
-contentDict[socialDir] = socialList
-contentDict[newsDir] = newsList
 
-# add file entries to content list
-for key, value in contentDict.items():
-    for subdir, dirs, files in os.walk(key):
-        for File in files:
-            contentDict[key].append(File)
+for subdir, dirs, files in os.walk(contentDir):
+    key = subdir.replace(contentDir + '/', '')
+    # 如果key不是contentDir目录，且key不是二级子目录（如果是的话，那么其中一定有/）
+    if key != '' and key.find('/') == -1:
+        contentDict[key] = []
+
+        # 遍历字目录，并且把字目录里的文件加到列表中
+        for root, d, filenames in os.walk(subdir):
+            for f in filenames:
+                contentDict[key].append(f)
 
 
-# 建立static目录和content内容的联系
+
+# 同样的方法组织staticDir的内容
+staticDir = "./static"
 staticDict = dict()
-scienceFiles = "./static/science/"
-socialFiles = "./static/social/"
-newsFiles = "./static/news/"
 
-staticDict[scienceFiles] = scienceDir
-staticDict[socialFiles] = socialDir
-staticDict[newsFiles] = newsDir
+# 在黑名单中的文件夹也不会被列入，像是image等文件夹
+blackList = ['image']
 
-# 遍历static目录下文件
-for key, value in staticDict.items():
+for subdir, dirs, files in os.walk(staticDir):
+    key = subdir.replace(staticDir + '/', '')
+    # 如果key不是staticDir目录，且key不是二级子目录（如果是的话，那么其中一定有/）
+    if key != '' and key.find('/') == -1 and key not in blackList:
+        staticDict[key] = []
 
-    contentDir = staticDict[key]
-
-    for subdir, dirs, files in os.walk(key):
-        os.chdir(contentDir)
-        contentList = contentDict[contentDir]
-
-        # 如果static目录下文件在content对应目录没有文件，那么在content目录创建文件。
-        for File in files:
-            # File is XXXXX.html
-            title = File.split(".")
-
-            # markdown file Name
-            FileName = title[0] + ".md"
-
-            # 如果当前markdown文件在content里没有，那么就创建
-            if FileName not in contentList:
-                # 构建front matter
-                frontMatter = constructFrontMatter(title[0], date, [""], [""])
-                link = File.replace(" ", "%20")
-                contentPath = contentDir.split("/")
-
-                # 创建文件并写入内容
-                with open(FileName, "w", encoding="UTF-8") as f:
-                    f.write(frontMatter)
-                    f.write('\n')
-                    # file content
-                    content = "[" + File + "]" + "(/" + str(contentPath[2]) + "/" + link + ")\n"
-
-                    f.write(content)
-                    
-        os.chdir(iniDir)
+        # 遍历子目录，并且把字目录里的文件加到列表中
+        for root, d, filenames in os.walk(subdir):
+            for f in filenames:
+                staticDict[key].append(f)
 
 
 
+for key in staticDict:
+    curDir = contentDir + '/' + key
+    if key not in contentDict.keys():
+        contentDict[key] = list()
+        os.mkdir(curDir)
 
-        
-        
+    os.chdir(curDir)
 
+    for item in staticDict[key]:
 
+        # 文件名字叫XXXX.html，按照.进行分割
+        title = item.split(".")
+
+        # markdown file Name
+        FileName = title[0] + ".md"
+
+        if FileName not in contentDict[key]:
+            # 构建front matter
+            frontMatter = constructFrontMatter(title[0], date, [""], [key])
+
+            link = item.replace(" ", "%20")
+
+            # 创建文件并写入内容
+            with open(FileName, "w", encoding="UTF-8") as f:
+                f.write(frontMatter)
+                f.write('\n')
+
+                # file content
+                content = "[" + title[0] + "]" + "(/" + str(key) + "/" + link + ")\n"
+
+                f.write(content)
+
+    os.chdir(iniDir)
     
-
 
